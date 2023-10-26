c8376fb7f9b52e1bb598cfe090ac2d3f59cbe9b0
Pohuș Tabita <emanuela.pohus@emanuel.ro>
2023-10-25 23:07:49 -1200

    verificare

---
+++ sistem_audentificare/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace sistem_audentificare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                const string usernamecorect = "pohus.tabita18@gmail.com";
+                const string parolacorecta = "tabitapohusemanuela18";
+                bool adevarat = false;
+                while (!adevarat)
+                {
+                    Console.WriteLine("Introduceti username");
+                    string username = Console.ReadLine();
+                    Console.WriteLine("Introduceti parola");
+                    string parola = Console.ReadLine();
+                    if (username == usernamecorect && parola == parolacorecta)
+                    {
+                        Console.WriteLine("Bine ai venit" + usernamecorect);
+                        adevarat = true;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Incorect");
+                    }
+                }
+            }
+        }
+    }
+}
